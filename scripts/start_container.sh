b16269d5a1fe8a7a6ecadbd1e63f83b4ceb24da7
rafindada <64506347+rafindada@users.noreply.github.com>
2023-11-21 15:47:55 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull rafindada/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 rafindada/simple-python-flask-app
